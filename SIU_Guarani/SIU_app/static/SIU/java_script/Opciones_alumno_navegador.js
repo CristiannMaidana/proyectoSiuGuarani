8c09029cc9fc0f64528191112446e283b418505f
CristiannMaidana <cristian.mg.david@gmail.com>
2024-09-27 22:19:23 -0300

    Se creo el archivo "Opciones_alumno_navegador.js" en la carpeta "java_script" en "static" .

---
+++ SIU_Guarani/SIU_app/static/SIU/java_script/Opciones_alumno_navegador.js
@@ -0,0 +1,17 @@
+    function mostrarOpciones_alumno() {
+        let listaOpciones = document.getElementById("lista_opciones_alumno");
+        if (listaOpciones.style.display === "none") {
+            listaOpciones.style.display = "block";
+        } else {
+            listaOpciones.style.display = "none";
+        }
+    }
+
+    function mostrarOpciones_navegador() {
+        let listaOpciones = document.getElementById("lista_opciones_navegador");
+        if (listaOpciones.style.display === "none") {
+            listaOpciones.style.display = "block";
+        } else {
+            listaOpciones.style.display = "none";
+        }
+    }
